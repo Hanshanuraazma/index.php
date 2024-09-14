01548e5171ff9d85d455d448a6d63e3133b14ef1
Hanshanuraazma <nunu05867@gmail.com>
2024-09-14 22:40:11 +0700

    Add files via upload

--- send_message.php
+++ send_message.php
@@ -2,16 +2,16 @@
   $to = 'nunu05867@gmail.com'; 
   $subject = 'Kembar Fried Chicken';
   
-  $message = 'Halo,\n\n' .
-             'Terima kasih telah menghubungi Kembar Fried Chicken. Kami senang bisa melayani Anda.\n\n' .
-             'Salam hangat,\n' .
-             'Tim Kembar Fried Chicken\n' .
-             'Alamat: Kp. Parakan Sagu, Kec. Pamijahan, Kab. Bogor\n' .
+  $message = 'Halo,\n\n' 
+             'Terima kasih telah menghubungi Kembar Fried Chicken. Kami senang bisa melayani Anda.\n\n' 
+             'Salam hangat,\n' 
+             'Tim Kembar Fried Chicken\n' 
+             'Alamat: Kp. Parakan Sagu, Kec. Pamijahan, Kab. Bogor\n' 
              'Telepon: 085882508760\n';
 
-  $headers = 'From: kembarfriedchicken@gmail.com' . "\r\n" .
-             'Reply-To: kembarfriedchicken@gmail.com' . "\r\n" .
-             'X-Mailer: PHP/' . phpversion();
+  $headers = 'From: kembarfriedchicken@gmail.com'  "\r\n" 
+             'Reply-To: kembarfriedchicken@gmail.com'  "\r\n" 
+             'X-Mailer: PHP/'  phpversion();
 
   if (mail($to, $subject, $message, $headers)) {
       echo 'Email berhasil dikirim!';
